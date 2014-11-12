3ef7324f6778302ce13e6608480f071de1c5a83d
Adam Brenecki <adam@brenecki.id.au>
2014-11-12 13:49:31 +1030

    feat(protocol): Support for protocol change

--- srfp/protocol.py
+++ srfp/protocol.py
@@ -1,10 +1,19 @@
 import struct
 from binascii import crc32
+import six
+
+import logging
+logger = logging.getLogger(__name__)
 
 _HEADER_SPEC = '!BHH'
 _FOOTER_SPEC = '!I'
 HEADER_LEN = struct.calcsize(_HEADER_SPEC)
 FOOTER_LEN = struct.calcsize(_FOOTER_SPEC)
+
+def _make_bytes(stri):
+    if type(stri) != six.binary_type:
+        return stri.encode('utf8')
+    return stri
 
 class Message (object):
     @classmethod
@@ -18,8 +27,7 @@ class Message (object):
         msgtype, id, length = struct.unpack(_HEADER_SPEC, header)
 
         if cls == Message:
-            subcls = (REQUEST_CLASSES if not response
-                    else RESPONSE_CLASSES)[msgtype]
+            subcls = MESSAGE_CLASSES[msgtype]
         else:
             subcls = cls
 
@@ -33,6 +41,7 @@ class Message (object):
         expected_crc = crc32(data[:-FOOTER_LEN], crc32(self.raw_header))
         (actual_crc,) = struct.unpack(_FOOTER_SPEC, data[-FOOTER_LEN:])
         # TODO: check CRCs
+        logger.debug("header:%s body:%s crc:%s", repr(self.raw_header), repr(data)[:30], repr(actual_crc))
         self._parse_message_body(data[:-FOOTER_LEN])
 
     def encode(self, id):
@@ -40,6 +49,7 @@ class Message (object):
         header = struct.pack(_HEADER_SPEC, self.msgtype, id, len(body))
         raw_crc = crc32(body, crc32(header)) & 0xffffffff
         crc = struct.pack(_FOOTER_SPEC, raw_crc)
+        logger.debug("header:%s body:%s crc:%s", repr(header), repr(body)[:30], repr(crc))
         return b''.join((header, body, crc))
 
 
@@ -47,7 +57,7 @@ class DirectoryListRequest (Message):
     msgtype = 0x01
 
     def __init__(self, path=[]):
-        self.path = path
+        self.path = [_make_bytes(i) for i in path]
 
     def _parse_message_body(self, body):
         self.path = body.split(b'\0')
@@ -56,7 +66,7 @@ class DirectoryListRequest (Message):
         return b'\0'.join(self.path)
 
 class DirectoryListResponse (Message):
-    msgtype = 0x01
+    msgtype = 0x81
 
     def __init__(self, listing=[]):
         self.listing = listing
@@ -71,7 +81,7 @@ class NodeInfoRequest (Message):
     msgtype = 0x02
 
     def __init__(self, path=[]):
-        self.path = path
+        self.path = [_make_bytes(i) for i in path]
 
     def _parse_message_body(self, body):
         self.path = body.split(b'\0')
@@ -80,7 +90,7 @@ class NodeInfoRequest (Message):
         return b'\0'.join(self.path)
 
 class NodeInfoResponse (Message):
-    msgtype = 0x02
+    msgtype = 0x82
 
     def __init__(self, isfile=False, size=0, created=0, accessed=0, modified=0):
         self.isfile = isfile
@@ -103,7 +113,7 @@ class FileContentsRequest (Message):
     def __init__(self, offset=0, requested_length=0, path=[]):
         self.offset = offset
         self.requested_length = requested_length
-        self.path = path
+        self.path = [_make_bytes(i) for i in path]
 
     def _parse_message_body(self, body):
         self.offset, self.requested_length = struct.unpack('!II', body[:8])
@@ -114,7 +124,7 @@ class FileContentsRequest (Message):
                 b'\0'.join(self.path))
 
 class FileContentsResponse (Message):
-    msgtype = 0x03
+    msgtype = 0x83
 
     def __init__(self, data=None):
         self.data = data
@@ -136,7 +146,7 @@ class VersionRequest (Message):
 
 class VersionResponse (Message):
     """The Python representation of the version should be a 3-tuple: (major, minor, bugfix)"""
-    msgtype = 0x7F
+    msgtype = 0xFF
 
     def __init__(self, version=None):
         self.version = version
@@ -147,15 +157,13 @@ class VersionResponse (Message):
     def _encode_message_body(self):
         return struct.pack('!BBB', *self.version)
 
-REQUEST_CLASSES = {
+MESSAGE_CLASSES = {
     0x01: DirectoryListRequest,
     0x02: NodeInfoRequest,
     0x03: FileContentsRequest,
     0x7F: VersionRequest,
-}
-RESPONSE_CLASSES = {
-    0x01: DirectoryListResponse,
-    0x02: NodeInfoResponse,
-    0x03: FileContentsResponse,
-    0x7F: VersionResponse,
+    0x81: DirectoryListResponse,
+    0x82: NodeInfoResponse,
+    0x83: FileContentsResponse,
+    0xFF: VersionResponse,
 }
